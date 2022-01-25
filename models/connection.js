502a6c626182a078f1059100d921f099b8461314
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-01-25 14:51:49 -0300

    Criaçao de diretorios base

--- models/connection.js
+++ models/connection.js
@@ -4,7 +4,6 @@ const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
 });
 
 module.exports = connection;
