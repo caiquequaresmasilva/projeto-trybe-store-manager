9c41b1d0714847a9e92bb8862780e18a660e567f
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-01-25 17:07:24 -0300

    Metodo getById no Model products

--- models/productsModel.js
+++ models/productsModel.js
@@ -18,8 +18,15 @@ const getAll = async () => {
   return products;
 };
 
+const getById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const [[product]] = await connection.execute(query, [id]);
+  return product;
+};
+
 module.exports = {
   create,
   getByName,
   getAll,
+  getById,
 };
