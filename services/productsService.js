3bdd8c5a5a7d28fd5f88167c44835238ce08dbe8
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-01-25 15:03:51 -0300

    Metodo create no Service products

--- services/productsService.js
+++ services/productsService.js
@@ -1,1 +1,13 @@
 const productsModel = require('../models/productsModel');
+
+const create = async ({ name, quantity }) => {
+  const product = await productsModel.getByName(name);
+  if (product) return { error: { code: 'alreadyExists', message: 'Product already exists' } };
+
+  const { id } = await productsModel.create({ name, quantity });
+  return { id, name, quantity };
+};
+
+module.exports = {
+  create,
+};
