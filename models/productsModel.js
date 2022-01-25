aae65095f08cc9e4a8c7db68f2cafa20ce883a33
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-01-25 14:56:10 -0300

    Metodo create no Model products

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,1 +1,11 @@
 const connection = require('./connection');
+
+const create = async ({ name, quantity }) => {
+  const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?,?)';
+  const [{ insertId: id }] = await connection.execute(query, [name, quantity]);
+  return { id };
+};
+
+module.exports = {
+  create,
+};
