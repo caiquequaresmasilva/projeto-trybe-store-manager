00c22b29d28210f7462acecb94dfaacea2692eda
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-01-25 17:29:55 -0300

    Requisito 2 finalizado

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,6 +4,8 @@ const productsController = require('../controllers/productsController');
 
 const router = express.Router();
 
+router.get('/', rescue(productsController.getAll));
+router.get('/:id', rescue(productsController.getById));
 router.post('/', rescue(productsController.validateProducts), rescue(productsController.create));
 
 module.exports = router;
